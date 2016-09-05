be6740bd3bce436f4011fbec60260452ce8b26fe
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-09-05 16:37:43 +1200

    Test

--- TestMoveAction/TestScene.swift
+++ TestMoveAction/TestScene.swift
@@ -13,12 +13,13 @@ class TestScene:SKScene {
         //シーンを画面サイズに合わせる。
         self.scaleMode = .AspectFit
         
+        //画面端に物理ボディを設定する。
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)        
     }
     
     
     //画面タッチ時の呼び出しメソッド
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("タップしました。")
+        print("タップしました")
     }
-
 }
